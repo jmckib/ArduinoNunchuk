3b9336b6299e3ff833c8ad38af38bfb26fa11f37
Jeremy McKibben-Sanders <jeremy@counsyl.com>
2012-07-24 01:31:06 -0700

    Nicer formatting of data in the example.

--- ArduinoNunchuk/examples/ArduinoNunchukDemo/ArduinoNunchukDemo.ino
+++ ArduinoNunchuk/examples/ArduinoNunchukDemo/ArduinoNunchukDemo.ino
@@ -25,17 +25,24 @@ void loop()
 {
   nunchuk.update();
 
+  Serial.print("\tjoyX: ");
   Serial.print(nunchuk.joyX, DEC);
-  Serial.print(' ');
+
+  Serial.print("\tjoyY: ");
   Serial.print(nunchuk.joyY, DEC);
-  Serial.print(' ');
+
+  Serial.print("\taccelX: ");
   Serial.print(nunchuk.accelX, DEC);
-  Serial.print(' ');
+
+  Serial.print("\taccelY: ");
   Serial.print(nunchuk.accelY, DEC);
-  Serial.print(' ');
+
+  Serial.print("\taccelZ: ");
   Serial.print(nunchuk.accelZ, DEC);
-  Serial.print(' ');
+
+  Serial.print("\tzButton: ");
   Serial.print(nunchuk.zButton, DEC);
-  Serial.print(' ');
+
+  Serial.print("\tcButton: ");
   Serial.println(nunchuk.cButton, DEC);
 }
